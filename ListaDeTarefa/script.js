ad81fe6999e91149ee63102f9feac38fd2d9cd7a
NatanaelSantos2 <154018545+NatanaelSantos2@users.noreply.github.com>
2024-01-15 06:47:57 -0300

    Add files via upload

---
+++ ListaDeTarefa/script.js
@@ -0,0 +1,26 @@
+const input = document.getElementById('input-todos');
+const saveBtn = document.getElementById('save');
+const lista = document.getElementById('lista');
+
+saveBtn.addEventListener("click", function () {
+    if (input.value !== "") {
+        let text = input.value;
+        let quant = document.querySelectorAll("li").length;
+        let html = `
+            <li id="${quant}">
+                <input type="checkbox">
+                <p>${text}</p>
+                <button onclick="remover(${quant})" type="button">remove</button>
+            </li>
+        `;
+        lista.innerHTML += html;
+        input.value = "";
+
+    } else {
+        input.style.border = "1px solid red";
+    }
+});
+
+input.addEventListener("keydown", function () {
+    input.style.border = "1px solid #bdbdbd";
+});
